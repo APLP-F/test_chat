9c60404e462e7c64b117c04265a6da283f877fb8
Jose Luis Larrea Díaz - GEMED <jlarrea@gemed.io>
2026-07-23 14:32:41 +0100

    Actualizar redirect URI GitHub Pages

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -3,8 +3,10 @@ export const msalConfig = {
     clientId: "84ef3bdb-a4d5-4094-8e8f-c763b1a3edbb",
     authority:
       "https://login.microsoftonline.com/436efeb8-20ec-47a9-9dbb-3deab23539f5",
-    redirectUri: "http://localhost:5173",
+
+    redirectUri: "https://aplp-f.github.io/test_chat/",
   },
+
   cache: {
     cacheLocation: "sessionStorage",
     storeAuthStateInCookie: false,
