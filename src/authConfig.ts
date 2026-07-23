89140af9c1d69a8447c83ad939a3b9d896b07e16
Jose Luis Larrea Díaz - GEMED <jlarrea@gemed.io>
2026-07-23 15:32:17 +0100

    Agregar auth callback para popup MSAL

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,10 +1,16 @@
+const isGitHubPages =
+  window.location.hostname === "aplp-f.github.io";
+
+const redirectUri = isGitHubPages
+  ? "https://aplp-f.github.io/test_chat/auth.html"
+  : "http://localhost:5173/auth.html";
+
 export const msalConfig = {
   auth: {
     clientId: "84ef3bdb-a4d5-4094-8e8f-c763b1a3edbb",
     authority:
       "https://login.microsoftonline.com/436efeb8-20ec-47a9-9dbb-3deab23539f5",
-
-    redirectUri: "https://aplp-f.github.io/test_chat/",
+    redirectUri,
   },
 
   cache: {
